ab2a4543e0bea081c847e1390972387ef121fa7e
gdozdjov <german.dozdjov@gmail.com>
2024-03-25 21:22:40 +0200

    --

--- cypress/e2e/main_task.cy.js
+++ cypress/e2e/main_task.cy.js
@@ -1,6 +1,25 @@
 describe('Main Task', () => {
-  it('passes', () => {
+
+  beforeEach(() => {
+    cy.viewport(1280, 800)
+  });
+
+  it('Main Task 1-4', () => {
     cy.visit('https://www.playtech.ee')
-    cy.url().should('eq', 'https://www.playtech.ee/')
+    cy.get('a[href="/who-we-are"]').first().click()
+    cy.get('h2').contains('Global presence')
+      .should('exist')
+      .and('be.visble')
   })
+
+  it.only('Bonus Task 1', () => {
+    cy.visit('https://www.playtech.ee')
+    cy.get('a[href="/who-we-are"]').first()
+      .click(93, 80, { force: true })
+    cy.url().should('eq', 'https://www.playtech.ee/who-we-are')
+  })
+
+  it('Bonus Task 2', () => {
+    
+  });
 })
