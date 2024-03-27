1ae563743a0099d08e984925d5f84874b52c8a47
gdozdjov <german.dozdjov@gmail.com>
2024-03-27 12:39:02 +0200

    --

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+  reporter: "junit",
+
+  reporterOptions: {
+    mochaFile: "results/test-results.xml",
+    outputs: true,
+    testCaseSwitchClassnameAndName: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
--- cypress/e2e/main_task.cy.js
+++ cypress/e2e/main_task.cy.js
@@ -6,20 +6,21 @@ describe('Main Task', () => {
 
   it('Main Task 1-4', () => {
     cy.visit('https://www.playtech.ee')
-    cy.get('a[href="/who-we-are"]').first().click()
+    cy.get('a[href="/who-we-are"]').first().click()  
     cy.get('h2').contains('Global presence')
+      .scrollIntoView() //changes to opacity: 1
       .should('exist')
-      .and('be.visble')
-  })
+      .and('be.visible')
+  });
 
-  it.only('Bonus Task 1', () => {
+  it('Bonus Task 1', () => {
     cy.visit('https://www.playtech.ee')
     cy.get('a[href="/who-we-are"]').first()
       .click(93, 80, { force: true })
     cy.url().should('eq', 'https://www.playtech.ee/who-we-are')
-  })
+  });
 
   it('Bonus Task 2', () => {
-    
+
   });
-})
+});
